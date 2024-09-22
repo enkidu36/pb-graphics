fd4efe815bef5a308ddb03f999a6aec1263eb66e
Joseph Savoie <enkidu36@yahoo.com>
2024-09-21 23:19:13 -0500

    Added src/test directory with dgutils-test.cljs

---
+++ src/test/pbranes/graphics/common/dgutils_test.cljs
@@ -0,0 +1,25 @@
+(ns pbranes.graphics.common.dgutils-test
+  (:require [cljs.test :refer-macros [testing is deftest]]
+            [pbranes.graphics.common.dgutils :as dg]))
+
+(set! *warn-on-infer* false)
+
+(deftest normalize-color-test
+  (testing "Normalize color maps 0-255 to 0-1  "
+    (is (= [0 0 0] (dg/normalize-color [0 0 0])))
+    (is (= [1 1 1] (dg/normalize-color [255 255 255])))))
+
+(deftest de-normalize-color-test
+  (testing "Normalize color maps 0-255 to 0-1"
+    (is (= [0 0 0] (dg/de-normalize-color [0 0 0])))
+    (is (= [255 255 255] (dg/de-normalize-color [1 1 1])))))
+
+(deftest folder-test
+  (is (dg/folder? {})))
+
+(deftest action-test
+  (is (dg/action? #(prn "hi"))))
+
+(deftest testfn-test
+  (is (dg/testfn 5)))
+
